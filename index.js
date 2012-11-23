1cbb3359e272e3858090a1e601e251abf6c851cd
Amir Abu Shareb <amir.abu.shareb@me.com>
2012-11-23 21:32:26 +0200

    remove unneeded try's. closes #1

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 
 /**
- * Get new `XMLHttpRequest` obj.
+ * XMLHttpRequest / ActiveXObject
  *
  * example:
  *
- *        req = xhr();
+ *        var req = xhr();
  *
  * @return {Object}
  */
 
 module.exports = function () {
   if (window.XMLHttpRequest) return new XMLHttpRequest();
-  try{ return new ActiveXObject('Microsoft.XMLHTTP'); } catch(e){}
-  try{ return new ActiveXObject('Msxml2.XMLHTTP.6.0'); } catch(e){}
-  try{ return new ActiveXObject('Msxml2.XMLHTTP.3.0'); } catch(e){}
-  try{ return new ActiveXObject('Msxml2.XMLHTTP'); } catch(e){}
+  try{ return new ActiveXObject('msxml2.xmlhttp.6.0'); } catch(e){}
+  try{ return new ActiveXObject('msxml2.xmlhttp.3.0'); } catch(e){}
+  try{ return new ActiveXObject('msxml2.xmlhttp'); } catch(e){}
 };
