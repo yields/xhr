38e912d3d4ff1ad0a49f25ef0df75c7077383b9a
Amir Abu Shareb <yields@icloud.com>
2013-05-16 23:33:15 +0300

    Release 1.0.0

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
  * @return {Object}
  */
 
-module.exports = function () {
+module.exports = function(){
   if (window.XMLHttpRequest) return new XMLHttpRequest();
   try{ return new ActiveXObject('msxml2.xmlhttp.6.0'); } catch(e){}
   try{ return new ActiveXObject('msxml2.xmlhttp.3.0'); } catch(e){}
